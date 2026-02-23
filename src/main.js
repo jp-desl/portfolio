7bb5bc29a77d2f76a90da0485d945ec49c3ed6d1
JP Desloges <jp.desloges@gmail.com>
2026-02-22 23:33:15 -0500

    New icons and use of SVGs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import "./styles.css";
 
+/* Icons */
+import tetraIcon from './assets/img/icon-tetra.svg?raw';
+document.querySelector('.c-icon__container.--tetra').innerHTML = tetraIcon;
+
+import cubeIcon from './assets/img/icon-cube.svg?raw';
+document.querySelector('.c-icon__container.--cube').innerHTML = cubeIcon;
+
+import octaIcon from './assets/img/icon-octa.svg?raw';
+document.querySelector('.c-icon__container.--octa').innerHTML = octaIcon;
+
+import dodecaIcon from './assets/img/icon-dodeca.svg?raw';
+document.querySelector('.c-icon__container.--doceca').innerHTML = dodecaIcon;
+
+import icosaIcon from './assets/img/icon-icosa.svg?raw';
+document.querySelector('.c-icon__container.--icosa').innerHTML = icosaIcon;
+
+
+
+
+/* Sections */
 import "./js/hero";
 import "./js/projects";
 import "./js/about";
@@ -7,8 +27,10 @@ import "./js/about";
 import "locomotive-scroll/dist/locomotive-scroll.css";
 import gsap from "gsap";
 import { TextPlugin } from "gsap/TextPlugin"
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import LocomotiveScroll from 'locomotive-scroll';
 
 gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(ScrollTrigger);
 
 const scroll = new LocomotiveScroll();
